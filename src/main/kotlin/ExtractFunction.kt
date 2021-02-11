973ef048f5493137245030ae3992562e3fe0b19d
J.Tadebois <jpm.tadebois@gmail.com>
2021-02-11 17:08:52 +0400

    Add example code for a suspend function

---
+++ src/main/kotlin/ExtractFunction.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    launch {
+        doWorld()
+    }
+    println("Hello,")
+}
+
+suspend fun doWorld() {
+    delay(1000L)
+    println("World")
+}
